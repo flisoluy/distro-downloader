64a2fdd368b1bda20c79cb74a4463d93c836c7d9
Francisco <13150712+fzadikian@users.noreply.github.com>
2025-04-20 01:10:00 -0300

    change some links downloader.sh

--- downloader.sh
+++ downloader.sh
@@ -52,10 +52,10 @@ wget -c https://tenet.dl.sourceforge.net/project/antix-linux/Final/antiX-23.2/ru
 wget -c https://tenet.dl.sourceforge.net/project/antix-linux/Final/antiX-23.2/antiX-23.2_386-full.iso?viasf=1
 wget -c https://tenet.dl.sourceforge.net/project/antix-linux/Final/antiX-23.2/antiX-23.2_x64-full.iso?viasf=1
 wget -c https://cdimage.ubuntu.com/lubuntu/releases/25.04/release/lubuntu-25.04-desktop-amd64.iso
-wget -c https://mirror.aarnet.edu.au/pub/xubuntu/releases/25.04/release/xubuntu-25.04-desktop-amd64.iso
+wget -c https://mirror.us.leaseweb.net/ubuntu-cdimage/xubuntu/releases/25.04/release/xubuntu-25.04-desktop-amd64.iso
 wget -c https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-12.10.0-amd64-standard.iso
-wget -c https://mirror.leaseweb.com/devuan/devuan_ascii/minimal-live/devuan_ascii_2.1_amd64_minimal-live.iso
-wget -c https://mirror.leaseweb.com/devuan/devuan_ascii/minimal-live/devuan_ascii_2.1_i386_minimal-live.iso
+wget -c https://mirror.leaseweb.com/devuan/devuan_daedalus/installer-iso/devuan_daedalus_5.0.1_amd64_netinstall.iso
+wget -c https://mirror.leaseweb.com/devuan/devuan_daedalus/installer-iso/devuan_daedalus_5.0.1_i386_netinstall.iso
 
 cd ..
 
